d89b5518d10200951d4c5e7c7dfb0c56bf4b7992
ssood123 <ss6358@columbia.edu>
2022-05-12 12:37:23 -0400

    Removed unnecessary comments

--- ast.ml
+++ ast.ml
@@ -85,7 +85,6 @@ let rec string_of_expr = function
   | Binop(e1, o, e2) ->
       string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
   | Unop(o, e) -> string_of_uop o ^ string_of_expr e
-  (* Assign(v, e) -> string_of_expr v ^ " = " ^ string_of_expr e *)
   | Assign(v, e) -> v ^ " = " ^ string_of_expr e
   | Call(f, el) ->
       f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
